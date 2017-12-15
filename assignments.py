e4e052edc771e80b8b5dece93cf5463928ebc3af
Avinash Kondareddy <avikndr@Avinashs-MacBook-Pro.local>
2017-12-15 12:45:41 -0800

    Wrote fad.snk

--- assignments.py
+++ assignments.py
@@ -1,13 +1,14 @@
-R2  : MSB 
-R3  : LSB
-R4  : Sign in MSB
-R5  : E
-R6  : E-val
-R7  : 
-R8  : 
-R9  : 
-R10 : 
-R11 : 
-R12 : R and S
+R0  : LD and ST address
+R2  : MSB of F1 -> MSB of final
+R3  : LSB of F1 -> LSB of final
+R4  : MSB of F2
+R5  : LSB of F2
+R6  : E of F1 -> E of larger
+R7  : E of F2 -> U bit 
+R8  : Sign bit of larger mantisssa
+R9  : (E1-E2) -> R bit
+R10 : (E2-E1) -> S bit
+R11 : (E1-E2)-(E2-E1)
+R12  : 
 R13 : 
 R14 : 
